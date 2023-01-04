a7c213a8973fb8bc16a5928c3fe15f66bd83cc73
vyshnavB <vyshnav.b17@gmail.com>
2023-01-04 18:35:46 +0530

    ok

--- network/static/layout.js
+++ network/static/layout.js
@@ -268,9 +268,9 @@ function like_post(element) {
         value++;
         count.innerHTML = value;
         element.querySelector('.svg-span').innerHTML = `
-            <svg width="1.1em" height="1.1em" viewBox="0 -1 16 16" class="bi bi-heart-fill" fill="#e0245e" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-            </svg>`;
+        <svg xmlns="http://www.w3.org/2000/svg" width="1.1em" height="1.1em" fill="#e0245e" class="bi bi-star-fill" viewBox="0 -1 16 16">
+        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+      </svg>`;
         element.setAttribute('onclick','unlike_post(this)');
     })
 }
@@ -286,9 +286,9 @@ function unlike_post(element) {
         value--;
         count.innerHTML = value;
         element.querySelector('.svg-span').innerHTML = `
-            <svg width="1.1em" height="1.1em" viewBox="0 -1 16 16" class="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-            </svg>`;
+        <svg class="liked"  class="bi bi-star" width="1.1em" height="1.1em" xmlns="http://www.w3.org/2000/svg"  fill="currentColor"  viewBox="0 -1 16 16">
+        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+      </svg>`;
         element.setAttribute('onclick','like_post(this)');
     })
 }
@@ -318,7 +318,7 @@ function unsave_post(element) {
     })
     .then(() => {
         element.querySelector('.svg-span').innerHTML = `
-        <svg width="1.1em" height="1.1em" viewBox="0.5 0 15 15" class="bi bi-bookmark" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <svg width="1.1em" height="1.1em" viewBox="0.5 0 15 15" class="bi bi-bookmark" fill="#000" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M8 12l5 3V3a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v12l5-3zm-4 1.234l4-2.4 4 2.4V3a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v10.234z"/>
         </svg>`;
         element.setAttribute('onclick','save_post(this)');
@@ -490,4 +490,29 @@ function goto_register() {
 
 function goto_login() {
     window.location.href = '/n/login';
+}
+
+const viewBtn = document.querySelector(".view-modal"),
+popu = document.querySelector(".popu"),
+close = popu.querySelector(".close"),
+field = popu.querySelector(".field"),
+input = field.querySelector("input"),
+copy = field.querySelector("button");
+viewBtn.onclick = ()=>{
+  popu.classList.toggle("show");
+}
+close.onclick = ()=>{
+  viewBtn.click();
+}
+copy.onclick = ()=>{
+  input.select(); //select input value
+  if(document.execCommand("copy")){ //if the selected text copy
+    field.classList.add("active");
+    copy.innerText = "Copied";
+    setTimeout(()=>{
+      window.getSelection().removeAllRanges(); //remove selection from document
+      field.classList.remove("active");
+      copy.innerText = "Copy";
+    }, 3000);
+  }
 }
